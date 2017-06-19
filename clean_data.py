127abf7b072594dce82c43a767a684b7bfca1019
tomatoiscoding <bbl_zuiaimessi@sina.com>
2017-06-19 23:01:23 +0800

    w2v

--- clean_data.py
+++ clean_data.py
@@ -49,6 +49,9 @@ for row in cursor:
 
 import re
 
+with open('/Users/bianbeilei/stupidNLP/filter.txt') as f:
+	content = f.readlines()
+
 myre = [
 	r'(?:@[\w_]+)',
 	r'(?:\#+[\w_]+[\w\'_\-]*[\w_]+)',
@@ -64,3 +67,107 @@ tokens_re = re.compile(r'('+'|'.join(myre)+')', re.VERBOSE | re.IGNORECASE)
 def tokenize(s):
     return tokens_re.findall(s)
 
+
+# clean review
+
+import pandas as pd
+
+train = pd.read_csv('/Users/bianbeilei/Downloads/rating.csv',header=0)
+
+# train.shape
+# train['outputText'][0]
+
+# remove punctuation and numbers
+
+import re
+
+review_letter = re.sub('[^a-zA-Z]', ' ', train['outputText'][0])
+
+# lower case
+
+review_lower = review_letter.lower()
+words = review_lower.split()
+
+from nltk.corpus import stopwords
+from bs4 import BeautifulSoup
+
+# remove stopwords
+
+words = [w for w in words if not w in stopwords.words("english")]
+
+def clean_review(raw_data):
+	review_text = BeautifulSoup(raw_data).get_text()
+	review_letters = re.sub("[^a-zA-Z]", " ", review_text)
+	words = review_letters.lower().split()
+	stops = set(stopwords.words("english"))
+	meaningful_words = [w for w in words if not w in stops]
+	return( " ".join( meaningful_words))
+
+num_reviews = train["outputText"].size
+
+clean_train_reviews = []
+
+for i in xrange(0, num_reviews):
+    clean_train_reviews.append(clean_review(train["outputText"][i]))
+
+# bag of words
+
+from sklearn.feature_extraction.text import CountVectorizer
+
+vectorizer = CountVectorizer(analyzer = "word",
+                             tokenizer = None,
+                             preprocessor = None,
+                             stop_words = None,
+                             max_features = 5000)
+
+train_data_features = vectorizer.fit_transform(clean_train_reviews)
+vocab = vectorizer.get_feature_names()
+
+import numpy as np
+dist = np.sum(train_data_features, axis=0)
+for tag, count in zip(vocab, dist):
+    print count, tag
+
+# train word2vec
+
+"""
+input is a list of sentences, each sentence is a list of words
+First, split a paragraph into sentences.
+Second, train word2vec
+"""
+# raw['summary'][0]
+import pands as pd
+import nltk.data
+tokenizer = nltk.data.load('tokenizers/punkt/english.pickle')
+
+raw = pd.read_csv('/Users/bianbeilei/Algeria_summary.csv', header=0)
+
+# raw.shape
+
+def sent_to_word(raw_sentence):
+	sent = re.sub("[^a-zA-Z]"," ", raw_sentence)
+	words = sent.lower().split()
+	return(words)
+
+def sum_to_sent(summary, tokenizer):
+	raw_sentences = tokenizer.tokenize(summary.strip())
+	sentences = []
+	if len(raw_sentences)>0:
+		for raw_sentence in raw_sentences:
+			single_sentence = sent_to_word(raw_sentence)
+			sentences.append(single_sentence)
+	return(sentences)
+
+all_sentences = []
+for sum in raw["summary"]:
+    all_sentences += sum_to_sent(sum, tokenizer)
+
+import logging
+from gensim.models import word2vec
+logging.basicConfig(format='%(asctime)s : %(levelname)s : %(message)s',
+    level=logging.INFO)
+
+model = word2vec.Word2Vec(all_sentences, size=100, min_count = 40,
+            window = 10, sample = 0.001)
+
+model.save(model_name)
